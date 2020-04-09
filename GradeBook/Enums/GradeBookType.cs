132cf9c7fd226ac650ed7120b90b61f192d93441
rpost-intertech <62145451+rpost-intertech@users.noreply.github.com>
2020-04-09 08:25:06 -0500

    Creating New GradeBookTypes
    Created new GradeBook types and added enum for GradeBookType

---
+++ GradeBook/Enums/GradeBookType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.Enums
+{
+    enum GradeBookType
+    {
+        Standard,
+        Ranked,
+        ESNU,
+        OneToFour,
+        SixPoint
+
+    }
+}
